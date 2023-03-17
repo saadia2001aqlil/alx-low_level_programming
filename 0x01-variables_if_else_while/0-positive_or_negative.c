4408324dc12ce8fcc19a83930ab48c3cb8ac5e21
saadia2001aqlil <saadia.aqlil@edu.uiz.ac.ma>
2023-03-17 15:11:23 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+float n;
+if(n>0){
+printf("%f is positive",n);
+}else if(n=0){
+printf("%f is zero",n);
+}else {
+printf("%f is negative",n);
+}
+return (0);
+}

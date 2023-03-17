eec045b53170e1ac1fc99259d13e90777470be03
saadia2001aqlil <saadia.aqlil@edu.uiz.ac.ma>
2023-03-17 11:36:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * Description:main function
- *Return : 0 if no errors
+ * Return : 0 if no errors
  */
 int main(void){
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-
 }
         
